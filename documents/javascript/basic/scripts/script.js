2d62ef7d64a190a5e18b1e0b551bbb05d00e1263
Zavier <zmyles@comcast.net>
2018-07-23 16:02:14 -0400

    The battle begins

---
+++ documents/javascript/basic/scripts/script.js
@@ -0,0 +1,11 @@
+const answer = prompt("Do you want to play a game?");
+
+if (answer.toUpperCase() === 'YES') {
+    const playName = prompt("What is your name Warrior?");
+
+    let playerHealth = 40;
+    let grant = 10;
+    
+    while ( )
+} 
+
